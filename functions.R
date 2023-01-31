dd2baa600163a99e2e5c1a813a3ada3f4e78f970
Bernardo C. G. da Silva <bernardocgdasilva@gmail.com>
2023-01-31 01:43:47 -0300

    Painel BR sem log

--- functions.R
+++ functions.R
@@ -26,15 +26,34 @@
 # _______________________________________________________________________________
 
 # fuction to download time series from BACEN API
-get_sgs <- function(api_url,nome_variavel){
+get_sgs <- function(api_url, nome_coluna){#,nome_variavel){
   x <- httr::GET(api_url)
   y <- jsonlite::fromJSON(rawToChar(x$content))
   y$data <- lubridate::dmy(y$data)
   y$valor <- as.double(y$valor)
-  colnames(y) <- c("data", "valor")
-  y <- mutate(y, variavel = nome_variavel)
-  z <- tsibble::as_tsibble(y, index = data, key = variavel)
-  z <- arrange(z, data)
-  z <- z %>% fill_gaps(.full = FALSE) %>% fill(valor)
-  return(z)
+  colnames(y) <- c("data", nome_coluna)
+  #y <- mutate(y, variavel = nome_variavel)
+  #z <- tsibble::as_tsibble(y, index = data, key = variavel)
+  #z <- arrange(z, data)
+  #z <- z %>% fill_gaps(.full = FALSE) %>% fill(valor)
+  return(y)
+}
+
+# Transform to timeseries
+make_ts_month <- function(base){
+  x <- as.matrix(base$valor)
+  ano <- min(lubridate::year(base$data))
+  mes <- lubridate::month(min(base$data))
+  y <- ts(x, frequency = 12, start = c(ano, mes))
+  return(y)
+}
+
+# Convert from dail to monthly and keeps last daily value
+day_to_month <- function(base){
+  nome_coluna <- colnames(base)[2]
+  x <- base %>%
+    mutate(mes = lubridate::floor_date(data, "month")) %>%
+    group_by(mes) %>%
+    summarise(valor = last(enexpr(nome_coluna)))
+  return(x)
 }
--- main.R
+++ main.R
@@ -14,15 +14,46 @@ source("functions.R")
 
 # get data
 
-dolar_compra <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json", "dolar_compra")
-dolar_venda <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados?formato=json", "dolar_venda")
+#dolar_compra_diario <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json","dolar_compra_diario")#, "dolar_compra")
+#dolar_venda_diario <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados?formato=json","dolar_venda_diario")#, "dolar_venda")
+dolar_compra_mensal <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.3695/dados?formato=json", "dolar_compra_mensal")
+dolar_venda_mensal <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.3695/dados?formato=json", "dolar_venda_mensal")
+#M1_diario <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.27785/dados?formato=json","M1_diario")#, "base_monetaria_m1")
+M1_mensal <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.27791/dados?formato=json", "M1_mensal")
+#M1_defasado_diario <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.1821/dados?formato=json", "base_monetaria_m1")
+inflacao_mensal <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json", "inflacao_mensal")#, "ipca")
+selic_diario <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados?formato=json", "selic_diario")
+pib_mensal <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.4380/dados?formato=json", "pib_mensal")
+
+# Daily data to monthly data: last value
+
+selic_mensal <- selic_diario %>%
+  mutate(mes = lubridate::floor_date(data, "month")) %>%
+  group_by(data) %>%
+  summarise(selic_mensal = last(selic_diario))
+
+#####a <- day_to_month(selic_diario)
+
+# Build panel
+
+painel <- reduce(list(dolar_compra_mensal, dolar_venda_mensal, M1_mensal,inflacao_mensal, selic_mensal, pib_mensal),full_join, by = "data")
+painel <- painel[rowSums(is.na(painel)) == 0,]
+#teste <- painel[painel$dolar_compra_mensal != painel$dolar_venda_mensal,] # Para averiguar se há diferença entre os valores de compra e venda do dólar.
+
+dolar_ts <- make_ts_month(dolar_compra_mensal)
+M1_ts <- make_ts_month(M1_mensal)
+inflacao_ts <- make_ts_month(inflacao_mensal)
+pib_ts <- make_ts_month(pib_mensal)
+
+a <- lm(data = log(painel[,-c(1,2)]), formula = dolar_venda_mensal  ~.)
 
 
 fit <- fabletools::model(filter(dolar_compra, data >= lubridate::ymd("2000/01/01")),
                          arima = ARIMA(valor),
                          ets = ETS(valor),
                          theta = THETA(valor),
-                         RW = SNAIVE(valor)
+                         RW = NAIVE(valor),
+                         RWs = SNAIVE(valor)
                          )
 
 fit %>% accuracy()

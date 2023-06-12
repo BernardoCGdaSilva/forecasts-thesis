92043d5dd4f8790ed40a4f17aacbc5edf82df3f9
Bernardo C. G. da Silva <bernardocgdasilva@gmail.com>
2023-06-12 01:08:51 -0300

    BRICS data panel

--- 00_functions.R
+++ 00_functions.R
@@ -6,10 +6,12 @@
 # __________________________________ PACKAGES __________________________________
 # _____________________________________________________________________________
 {
-  pacotes_necessarios <- list("lubridate", "tidyverse", "httr",
-                              "jsonlite", "tsibble", "fable", "feasts", "sidrar",
-                              "ipeadatar", "neverhpfilter", "caret", "doParallel",
-                              "fredr", "tempdisagg")
+  pacotes_necessarios <- list(
+    "lubridate", "tidyverse", "httr",
+    "jsonlite", "tsibble", "fable", "feasts", "sidrar",
+    "ipeadatar", "neverhpfilter", "caret", "doParallel",
+    "fredr", "tempdisagg", "imfr"
+  )
 
   for (i in pacotes_necessarios) {
     eval(bquote(
@@ -28,21 +30,21 @@
 # _______________________________________________________________________________
 
 # fuction to download time series from BACEN API
-get_sgs <- function(api_url, nome_coluna){#,nome_variavel){
+get_sgs <- function(api_url, nome_coluna) { # ,nome_variavel){
   x <- httr::GET(api_url)
   y <- jsonlite::fromJSON(rawToChar(x$content))
   y$data <- lubridate::dmy(y$data)
   y$valor <- as.double(y$valor)
   colnames(y) <- c("date", nome_coluna)
-  #y <- mutate(y, variavel = nome_variavel)
-  #z <- tsibble::as_tsibble(y, index = data, key = variavel)
-  #z <- arrange(z, data)
-  #z <- z %>% fill_gaps(.full = FALSE) %>% fill(valor)
+  # y <- mutate(y, variavel = nome_variavel)
+  # z <- tsibble::as_tsibble(y, index = data, key = variavel)
+  # z <- arrange(z, data)
+  # z <- z %>% fill_gaps(.full = FALSE) %>% fill(valor)
   return(y)
 }
 
 # Transform to timeseries
-make_ts_month <- function(base){
+make_ts_month <- function(base) {
   x <- as.matrix(base$valor)
   ano <- min(lubridate::year(base$data))
   mes <- lubridate::month(min(base$data))
@@ -51,12 +53,80 @@ make_ts_month <- function(base){
 }
 
 # Convert from dail to monthly and keeps last daily value
-day_to_month <- function(base){
+day_to_month <- function(base) {
   nome_coluna <- colnames(base)[2]
   x <- base %>%
     dplyr::mutate(mes = lubridate::floor_date(date, "month")) %>%
     group_by(mes) %>%
-    summarise(valor = last(!! rlang::ensym(nome_coluna)))
-  #colnames(x) <- c("data", "valor")
+    summarise(valor = last(!!rlang::ensym(nome_coluna)))
+  # colnames(x) <- c("data", "valor")
+  return(x)
+}
+
+# Calculate output gap via Hamilton Filter from IMF GDP Data panel, quarterly data
+output_gap_q <- function(data, filtro = "BR", colname = "br_gap") {
+  gdp <- data %>%
+    dplyr::filter(ref_area == filtro) %>%
+    dplyr::select(c("date", "value"))
+  gdp$value <- as.double(gdp$value)
+  gdp$date <- stringr::str_remove_all(gdp$date, "Q") %>%
+    zoo::as.yearqtr() %>%
+    as.Date(frac = 1)
+  hf <- neverhpfilter::yth_filter(xts::as.xts(gdp), h = 8, p = 4)
+  gap <- gdp$value / hf$value.trend
+  gap <- as.data.frame(gap) %>%
+    rownames_to_column() %>%
+    `colnames<-`(c("date", colname))
+  gap$date <- lubridate::ymd(gap$date)
+  return(gap)
+}
+
+# Calculate output gap via Hamilton Filter, monthly data
+output_gap_m <- function(data, colname = "br_gap") {
+  hf <- neverhpfilter::yth_filter(xts::as.xts(data), h = 24, p = 12)
+  gap <- data$value / hf$value.trend
+  gap <- as.data.frame(gap) %>%
+    rownames_to_column() %>%
+    `colnames<-`(c("date", colname))
+  gap$date <- lubridate::ymd(gap$date)
+  return(gap)
+}
+
+# disaggregate data from lower frequency to higher (quarterly to monthly)
+quartet_to_monthly <- function(data_gdp, data_ip, filtro = "BR", colname="br_gdp", gdp_ano_0, gdp_quarter_0, ip_ano_0, ip_mes_0) {
+  gdp <- data_gdp %>%
+    filter(ref_area == filtro) %>%
+    select(c(date, value))
+  gdp$value <- as.double(gdp$value)
+  gdp$date <- stringr::str_remove_all(gdp$date, "Q") %>%
+    zoo::as.yearqtr() %>%
+    as.Date()
+  gdp_ts <- ts(gdp$value, start = c(gdp_ano_0, gdp_quarter_0), frequency = 4)
+  ip_ts <- ts(data_ip$value, start = c(ip_ano_0, ip_mes_0), frequency = 12) %>% forecast::na.interp()
+
+  gdp_monthly_fit <- tempdisagg::td(gdp_ts ~ ip_ts, conversion = "first", to = "month") %>% predict()
+  gdp_monthly <- data.frame(date = as.Date(time(gdp_monthly_fit)), value = as.matrix(gdp_monthly_fit))
+
+  gdp_monthly <- gdp_monthly %>% filter(date >= min(gdp$date) & date <= max(gdp$date))
+
+  plot <- ggplot() +
+    geom_line(gdp_monthly, mapping = aes(x = date, y = value, color = "monthly")) +
+    geom_line(gdp, mapping = aes(x = date, y = value, color = "quartely")) +
+    #tidyquant::coord_x_date(xlim = c("2018-01-01", max(gdp$date)))+
+    scale_color_manual(name = "", values = c("monthly" = "blue", "quartely" = "red")) +
+    labs(title = filtro)
+
+  #colnames(gdp_monthly) <- c("date", colname)
+
+  print(plot)
+  return(gdp_monthly)
+}
+
+# filter IFS exchage rate panel by country
+filter_ifs <- function(data, filter = "BR", colname = "br_exchange"){
+  x <- filter(data, ref_area == filter) %>% select(c(date,value))
+  x$date <- lubridate::ym(x$date)
+  x$value <- as.double(x$value)
+  colnames(x) <- c("date", colname)
   return(x)
 }
--- 01_data.R
+++ 01_data.R
@@ -1,113 +1,280 @@
-# TCC forecast: get data and build panel
+# TCC forecast: Get and organise data
 # Bernardo Cainelli Gomes da Silva
-# Nov - 2022
+# Jun - 2023
 
-# _________________________________________________________________________________
-# _____________________________ HELPER FUNCTIONS __________________________________
-# _________________________________________________________________________________
-
+# _____________________________________________________________________________
+# _______________________________ HELPER FUNCTIONS ____________________________
+# _____________________________________________________________________________
 source("00_functions.R")
 fredr_set_key("97f79f87ad234e695b64f0cc576fb23f")
 
-# _________________________________________________________________________________
-# __________________________________ DATA _________________________________________
-# _________________________________________________________________________________
+# View(imf_databases())
+# View(imf_parameters(database_id = "MFS"))
+# View(imf_parameters(database_id = "MFS")[[3]])
 
-# Exchange Rate
+# _____________________________________________________________________________
+# ________________________________ GET DATA ___________________________________
+# _____________________________________________________________________________
 
-br_exchange <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.3695/dados?formato=json", "br_exchange")
-#dolar_venda_mensal <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.3695/dados?formato=json", "dolar_venda_mensal")
+# nominal exchange rate, NSA
+nominal_exchange <- imf_dataset(
+  database_id = "IFS",
+  indicator = "ENDE_XDC_USD_RATE",
+  freq = "M",
+  ref_area = c("BR", "RU", "IN", "CN", "ZA")
+)
 
-# Brazilian Inflation
+br_exchange <- filter_ifs(nominal_exchange, filter = "BR", colname = "br_exchange")
+ru_exchange <- filter_ifs(nominal_exchange, filter = "RU", colname = "ru_exchange")
+in_exchange <- filter_ifs(nominal_exchange, filter = "IN", colname = "in_exchange")
+cn_exchange <- filter_ifs(nominal_exchange, filter = "CN", colname = "cn_exchange")
+za_exchange <- filter_ifs(nominal_exchange, filter = "ZA", colname = "za_exchange")
 
-br_inflation <- sidrar::get_sidra(x = 1737, variable = 2266, period = "all", geo = "Brazil")
-br_inflation <- br_inflation %>%
-  dplyr::mutate(data = ym(`Mês (Código)`)) %>%
-  dplyr::select(data, Valor)
-colnames(br_inflation) <- c("date", "br_inflation")
-
-# US Inflation
-
-us_inflation <- fredr::fredr(series_id = "CPIAUCNS") %>% select(1,3)
-colnames(us_inflation) <- c("date", "us_inflation")
-
-# Brazilian GDP
-
-br_gdp <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.4380/dados?formato=json", "br_gdp")
-br_gdp$br_gdp <- br_gdp$br_gdp * 1000000
-
-# US GDP: quarterly data turned monthly with Industrial Production as indicator (Riberio, 2017, p. 899)
-
-us_gdp <- fredr::fredr(series_id = "NA000334Q")
-us_gdp$value <- us_gdp$value*1000000
-us_gdp <- ts(us_gdp$value, start = c(1947,1), frequency = 4)
-
-us_ip <- fredr::fredr(series_id = "IPB50001N")
-us_ip <- ts(us_ip$value, start = c(1919,1), frequency = 12)
-
-us_gdp_monthly_fit <- tempdisagg::td(us_gdp ~ us_ip, conversion = "first", to = "month") %>% predict()
-us_gdp_monthly <- data.frame(date=as.Date(time(us_gdp_monthly_fit)), us_gdp=as.matrix(us_gdp_monthly_fit))
-
-#test
-ggplot() +
-  geom_line(data = us_gdp_monthly, mapping = aes (x = date, y = us_gdp))+
-  geom_line(data = data.frame(date=as.Date(time(us_gdp)), us_gdp=as.matrix(us_gdp)), mapping = aes (x = date, y = us_gdp)) +
-  tidyquant::coord_x_date(xlim = c("2016-01-01", "2023-05-11"))
-
-tail(data.frame(date=as.Date(time(us_gdp)), usa_gdp=as.matrix(us_gdp)))
-tail(us_gdp_monthly)
-
-# BR Interest Rate
-
-br_interest <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados?formato=json", "br_interest")
-br_interest <- day_to_month(br_interest) %>% `colnames<-`(c("date", "br_interest"))
-
-# US Interest Rate
-
-us_interest <- fredr(series_id = "FEDFUNDS") %>% select(1,3)
+# interest rate, NSA
+# br_interest <- fredr(series_id = "BRALOCOSTORSTM") %>% select(1, 3) # selic
+br_interest <- fredr(series_id = "IRSTCI01BRM156N") %>% select(1, 3) # interbank <24h
+ru_interest <- fredr(series_id = "IRSTCI01RUM156N") %>% select(1, 3)
+# in_interest <- fredr(series_id = "INDLOCOSTORSTM") %>% select(1, 3) # selic india
+in_interest <- fredr(series_id = "IRSTCI01INM156N") %>% select(1, 3) # interbank <24h
+cn_interest <- fredr(series_id = "IRSTCI01CNM156N") %>% select(1, 3) # interbank <24h
+za_interest <- fredr(series_id = "IRSTCI01ZAM156N") %>% select(1, 3) # interbank <24h
+# us_interest <- fredr(series_id = "FEDFUNDS") %>% select(1, 3) # fed funds
+us_interest <- fredr(series_id = "IR3TIB01USM156N") %>% select(1, 3) # interbank <24h
+colnames(br_interest) <- c("date", "br_interest")
+colnames(ru_interest) <- c("date", "ru_interest")
+colnames(in_interest) <- c("date", "in_interest")
+colnames(cn_interest) <- c("date", "cn_interest")
+colnames(za_interest) <- c("date", "za_interest")
 colnames(us_interest) <- c("date", "us_interest")
 
-# BR monetary suply
 
-br_m1 <- get_sgs("http://api.bcb.gov.br/dados/serie/bcdata.sgs.27791/dados?formato=json", "br_m1")
-br_m1$br_m1 <- br_m1$br_m1 * 1000
 
-# US monetary suply
+# inflation, NSA
+inflation <- imf_dataset(
+  database_id = "CPI",
+  indicator = "PCPI_IX",
+  freq = "M",
+  ref_area = c("BR", "RU", "IN", "CN", "ZA", "US")
+)
+br_inflation <- filter_ifs(inflation, filter = "BR", colname = "br_inflation")
+ru_inflation <- filter_ifs(inflation, filter = "RU", colname = "ru_inflation")
+in_inflation <- filter_ifs(inflation, filter = "IN", colname = "in_inflation")
+cn_inflation <- filter_ifs(inflation, filter = "CN", colname = "cn_inflation")
+za_inflation <- filter_ifs(inflation, filter = "ZA", colname = "za_inflation")
+us_inflation <- filter_ifs(inflation, filter = "US", colname = "us_inflation")
 
-us_m1 <- fredr(series_id = "M1NS")%>% select(1,3)
+# M1, NSA
+br_m1 <- fredr(series_id = "MANMM101BRM189N") %>% select(1, 3)
+colnames(br_m1) <- c("date", "br_m1")
+ru_m1 <- fredr(series_id = "MANMM101RUM189N") %>% select(1, 3)
+colnames(ru_m1) <- c("date", "ru_m1")
+in_m1 <- fredr(series_id = "MANMM101INM189N") %>% select(1, 3)
+colnames(in_m1) <- c("date", "in_m1")
+cn_m1 <- fredr(series_id = "MANMM101CNM189N") %>% select(1, 3)
+colnames(cn_m1) <- c("date", "cn_m1")
+za_m1 <- fredr(series_id = "MANMM101ZAM189N") %>% select(1, 3)
+colnames(za_m1) <- c("date", "za_m1")
+us_m1 <- fredr(series_id = "MANMM101USM189N") %>% select(1, 3)
 colnames(us_m1) <- c("date", "us_m1")
-us_m1$us_m1 <- us_m1$us_m1 * 1000000000
 
-# BR output gap via Hamilton Filter
+# Industrial production: GDP proxy, monthly, NSA
 
-br_hf <- neverhpfilter::yth_filter(xts::as.xts(br_gdp), h = 24, p = 12)
-br_gap <- br_gdp$br_gdp / br_hf$br_gdp.trend
-br_gap <- as.data.frame(br_gap) %>%
-  rownames_to_column() %>%
-  `colnames<-`(c("date", "br_gap"))
-br_gap$date <- lubridate::ymd(br_gap$date)
+br_ip <- fredr(series_id = "BRAPRMNTO01IXOBM") %>% select(1, 3)
+ru_ip <- fredr(series_id = "RUSPRMNTO01IXOBM") %>% select(1, 3)
+# ru_ip <- fredr(series_id = "RUSPRINTO01IXOBM") %>% select(1, 3)
+# ru_ip <- fredr(series_id = "RUSSLRTTO02MLM") %>% select(1, 3)
+in_ip <- fredr(series_id = "INDPRMNTO01IXOBM") %>% select(1, 3)
+cn_ip <- fredr(series_id = "CHNPRINTO01IXPYM") %>% select(1, 3)
+za_ip <- fredr(series_id = "ZAFPRMNTO01IXOBM") %>% select(1, 3)
+us_ip <- fredr(series_id = "USAPRMNTO01IXOBM") %>% select(1, 3)
+us_ip <- us_ip[rowSums(is.na(us_ip)) == 0, ]
 
-# US output gap via Hamilton Filter
+# nominal gdp, quarterly, NSA
+nominal_gdp <- imf_dataset(
+  database_id = "IFS",
+  indicator = "NGDP_NSA_XDC",
+  freq = "Q",
+  ref_area = c("BR", "RU", "IN", "CN", "ZA", "US")
+)
 
-us_hf <- neverhpfilter::yth_filter(xts::as.xts(us_gdp_monthly), h = 24, p = 12)
-us_gap <- us_gdp_monthly$us_gdp / us_hf$us_gdp.trend
-us_gap <- as.data.frame(us_gap) %>%
-  rownames_to_column() %>%
-  `colnames<-`(c("date", "us_gap"))
-us_gap$date <- lubridate::ymd(us_gap$date)
+# Output gap via Hamilton Filter from quarterly series
 
-# _________________________________________________________________________________
-# _______________________________ BUILD PANEL _____________________________________
-# _________________________________________________________________________________
+# br_gap <- output_gap_q(data = nominal_gdp, filtro = "BR", colname = "br_gap")
+# ru_gap <- output_gap_q(data = nominal_gdp, filtro = "RU", colname = "ru_gap")
+# in_gap <- output_gap_q(data = nominal_gdp, filtro = "IN", colname = "in_gap")
+# cn_gap <- output_gap_q(data = nominal_gdp, filtro = "CN", colname = "cn_gap")
+# za_gap <- output_gap_q(data = nominal_gdp, filtro = "ZA", colname = "za_gap")
+# us_gap <- output_gap_q(data = nominal_gdp, filtro = "US", colname = "us_gap")
 
-data_panel <- reduce(list(br_exchange, br_inflation, us_inflation, br_gdp, us_gdp_monthly, br_interest, us_interest, br_m1, us_m1, br_gap, us_gap), full_join, by = "date")
-data_panel <- data_panel[rowSums(is.na(data_panel)) == 0, ]
-rownames(data_panel) <- seq(length = nrow(data_panel))
+# gdp: Quarterly to Monthly
 
-# Todo:
-# real exchange rate
-# diffs
+br_gdp <- quartet_to_monthly(
+  data_gdp = nominal_gdp,
+  data_ip = br_ip,
+  filtro = "BR",
+  # colname = "br_gdp",
+  gdp_ano_0 = 1996,
+  gdp_quarter_0 = 1,
+  ip_ano_0 = 1975,
+  ip_mes_0 = 1
+)
 
-write_csv2(data_panel, "outputs/Painel de dados.csv")
+ru_gdp <- quartet_to_monthly(
+  data_gdp = nominal_gdp,
+  data_ip = ru_ip,
+  filtro = "RU",
+  # colname = "ru_gdp",
+  gdp_ano_0 = 2003,
+  gdp_quarter_0 = 1,
+  ip_ano_0 = 1999,
+  ip_mes_0 = 1
+)
 
-rm(list = setdiff(ls(), "data_panel"))
+in_gdp <- quartet_to_monthly(
+  data_gdp = nominal_gdp,
+  data_ip = in_ip,
+  filtro = "IN",
+  # colname = "in_gdp",
+  gdp_ano_0 = 2004,
+  gdp_quarter_0 = 2,
+  ip_ano_0 = 1994,
+  ip_mes_0 = 4
+)
+
+cn_gdp <- quartet_to_monthly(
+  data_gdp = nominal_gdp,
+  data_ip = cn_ip,
+  filtro = "CN",
+  # colname = "cn_gdp",
+  gdp_ano_0 = 1992,
+  gdp_quarter_0 = 1,
+  ip_ano_0 = 1999,
+  ip_mes_0 = 1
+)
+
+za_gdp <- quartet_to_monthly(
+  data_gdp = nominal_gdp,
+  data_ip = za_ip,
+  filtro = "ZA",
+  # colname = "za_gdp",
+  gdp_ano_0 = 1993,
+  gdp_quarter_0 = 1,
+  ip_ano_0 = 1990,
+  ip_mes_0 = 1
+)
+
+us_gdp <- quartet_to_monthly(
+  data_gdp = nominal_gdp,
+  data_ip = us_ip,
+  filtro = "US",
+  # colname = "us_gdp",
+  gdp_ano_0 = 1950,
+  gdp_quarter_0 = 1,
+  ip_ano_0 = 1960,
+  ip_mes_0 = 1
+)
+
+# Output gap via Hamilton Filter from monthly series
+
+br_gap <- output_gap_m(data = br_gdp, colname = "br_gap")
+ru_gap <- output_gap_m(data = ru_gdp, colname = "ru_gap")
+in_gap <- output_gap_m(data = in_gdp, colname = "in_gap")
+cn_gap <- output_gap_m(data = cn_gdp, colname = "cn_gap")
+za_gap <- output_gap_m(data = za_gdp, colname = "za_gap")
+us_gap <- output_gap_m(data = us_gdp, colname = "us_gap")
+
+# Rename colnames of gdp here because if done before would break de functions
+colnames(br_gdp) <- c("date", "br_gdp")
+colnames(ru_gdp) <- c("date", "ru_gdp")
+colnames(in_gdp) <- c("date", "in_gdp")
+colnames(cn_gdp) <- c("date", "cn_gdp")
+colnames(za_gdp) <- c("date", "za_gdp")
+colnames(us_gdp) <- c("date", "us_gdp")
+
+# _____________________________________________________________________________
+# ______________________________ BUILD PANEL __________________________________
+# _____________________________________________________________________________
+
+# All the data
+panel_data <- reduce(
+  list(
+    br_exchange, br_gap, br_gdp, br_inflation, br_interest, br_m1,
+    ru_exchange, ru_gap, ru_gdp, ru_inflation, ru_interest, ru_m1,
+    in_exchange, in_gap, in_gdp, in_inflation, in_interest, in_m1,
+    cn_exchange, cn_gap, cn_gdp, cn_inflation, cn_interest, cn_m1,
+    za_exchange, za_gap, za_gdp, za_inflation, za_interest, za_m1,
+    us_gap, us_gdp, us_inflation, us_interest, us_m1
+  ),
+  full_join,
+  by = "date"
+)
+panel_data <- panel_data[rowSums(is.na(panel_data)) == 0, ]
+rownames(panel_data) <- seq(length = nrow(panel_data))
+
+# Brazilian data
+br_panel_data <- reduce(
+  list(
+    br_exchange, br_gap, br_gdp, br_inflation, br_interest, br_m1,
+    us_gap, us_gdp, us_inflation, us_interest, us_m1
+  ),
+  full_join,
+  by = "date"
+)
+br_panel_data <- br_panel_data[rowSums(is.na(br_panel_data)) == 0, ]
+rownames(br_panel_data) <- seq(length = nrow(br_panel_data))
+
+# Russian data
+ru_panel_data <- reduce(
+  list(
+    ru_exchange, ru_gap, ru_gdp, ru_inflation, ru_interest, ru_m1,
+    us_gap, us_gdp, us_inflation, us_interest, us_m1
+  ),
+  full_join,
+  by = "date"
+)
+ru_panel_data <- ru_panel_data[rowSums(is.na(ru_panel_data)) == 0, ]
+rownames(ru_panel_data) <- seq(length = nrow(ru_panel_data))
+
+# Indian data
+in_panel_data <- reduce(
+  list(
+    in_exchange, in_gap, in_gdp, in_inflation, in_interest, in_m1,
+    us_gap, us_gdp, us_inflation, us_interest, us_m1
+  ),
+  full_join,
+  by = "date"
+)
+in_panel_data <- in_panel_data[rowSums(is.na(in_panel_data)) == 0, ]
+rownames(in_panel_data) <- seq(length = nrow(in_panel_data))
+
+# Chinese data
+cn_panel_data <- reduce(
+  list(
+    cn_exchange, cn_gap, cn_gdp, cn_inflation, cn_interest, cn_m1,
+    us_gap, us_gdp, us_inflation, us_interest, us_m1
+  ),
+  full_join,
+  by = "date"
+)
+cn_panel_data <- cn_panel_data[rowSums(is.na(cn_panel_data)) == 0, ]
+rownames(cn_panel_data) <- seq(length = nrow(cn_panel_data))
+
+# South african data
+za_panel_data <- reduce(
+  list(
+    za_exchange, za_gap, za_gdp, za_inflation, za_interest, za_m1,
+    us_gap, us_gdp, us_inflation, us_interest, us_m1
+  ),
+  full_join,
+  by = "date"
+)
+za_panel_data <- za_panel_data[rowSums(is.na(za_panel_data)) == 0, ]
+rownames(za_panel_data) <- seq(length = nrow(za_panel_data))
+
+# Export
+write_csv2(panel_data, "outputs/panel_data.csv")
+write_csv2(br_panel_data, "outputs/br_panel_data.csv")
+write_csv2(ru_panel_data, "outputs/ru_panel_datas.csv")
+write_csv2(in_panel_data, "outputs/in_panel_data.csv")
+write_csv2(cn_panel_data, "outputs/cn_panel_data.csv")
+write_csv2(za_panel_data, "outputs/za_panel_data.csv")
